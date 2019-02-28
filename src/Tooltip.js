124678f39762e4517911c2880e4ee10a69685b9a
Oleksandr Pastukhov <flashka.ua@gmail.com>
2019-02-28 15:21:19 +0100

    Update Tooltip.js
    cleaned up Tooltip.js

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Trigger from 'rc-trigger';
-import { placements } from './placements';
-import Content from './Content';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Trigger from 'rc-trigger'
+import { placements } from './placements'
+import Content from './Content'
 
 class Tooltip extends Component {
   static propTypes = {
@@ -13,14 +13,14 @@ class Tooltip extends Component {
     placement: PropTypes.string,
     transitionName: PropTypes.oneOfType([
       PropTypes.string,
-      PropTypes.object,
+      PropTypes.object
     ]),
     animation: PropTypes.any,
     onVisibleChange: PropTypes.func,
     afterVisibleChange: PropTypes.func,
     overlay: PropTypes.oneOfType([
       PropTypes.node,
-      PropTypes.func,
+      PropTypes.func
     ]).isRequired,
     overlayStyle: PropTypes.object,
     overlayClassName: PropTypes.string,
@@ -31,8 +31,8 @@ class Tooltip extends Component {
     destroyTooltipOnHide: PropTypes.bool,
     align: PropTypes.object,
     arrowContent: PropTypes.any,
-    id: PropTypes.string,
-  };
+    id: PropTypes.string
+  }
 
   static defaultProps = {
     prefixCls: 'rc-tooltip',
@@ -42,11 +42,11 @@ class Tooltip extends Component {
     align: {},
     placement: 'right',
     trigger: ['hover'],
-    arrowContent: null,
-  };
+    arrowContent: null
+  }
 
   getPopupElement = () => {
-    const { arrowContent, overlay, prefixCls, id } = this.props;
+    const { arrowContent, overlay, prefixCls, id } = this.props
     return ([
       <div className={`${prefixCls}-arrow`} key="arrow">
         {arrowContent}
@@ -57,16 +57,16 @@ class Tooltip extends Component {
         prefixCls={prefixCls}
         id={id}
         overlay={overlay}
-      />,
-    ]);
+      />
+    ])
   }
 
   getPopupDomNode() {
-    return this.trigger.getPopupDomNode();
+    return this.trigger.getPopupDomNode()
   }
 
   saveTrigger = (node) => {
-    this.trigger = node;
+    this.trigger = node
   }
 
   render() {
@@ -79,11 +79,11 @@ class Tooltip extends Component {
       placement, align,
       destroyTooltipOnHide,
       defaultVisible, getTooltipContainer,
-      ...restProps,
-    } = this.props;
-    const extraProps = { ...restProps };
+      ...restProps
+    } = this.props
+    const extraProps = { ...restProps }
     if ('visible' in this.props) {
-      extraProps.popupVisible = this.props.visible;
+      extraProps.popupVisible = this.props.visible
     }
     return (
       <Trigger
@@ -109,8 +109,8 @@ class Tooltip extends Component {
       >
         {children}
       </Trigger>
-    );
+    )
   }
 }
 
-export default Tooltip;
+export default Tooltip
